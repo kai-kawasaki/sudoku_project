efa008578f67eba6d0179e7b7823894df2d44c51
wallinc776 <148251143+wallinc776@users.noreply.github.com>
2023-12-03 18:20:13 -0500

    Update cell.py
    PyGame-less pseudo-code

--- cell.py
+++ cell.py
@@ -1,14 +1,48 @@
 
 class Cell:
-
     def __init__(self, value, row, col, screen):
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
 
     def set_cell_value(self, value):
-        pass
+        self.value += 1
 
     def set_sketched_value(self, value):
-        pass
+        if self.value == 1:
+            pass  # graphic for 1
+        # ...
+        else:
+            pass # no graphic
 
-    def draw(self):
-        pass
+        def draw(self):
+            pass
+            # if mouse coordinate is from (self.col * cellsize, self.row * cellsize) to ((self.col + 1) * cellsize, (self.row + 1) * cellsize)
+                # color: red
+            # else
+                # color: black
+
+            # starting point (horrizontal lines)
+                # (self.row + i) * cellsize, self.col * cellsize
+                # i = 0,1
+                # color
+            # ending point
+                # (self.row + i) * cellsize, (self.col + 1) * cellsize
+                # i = 0,1
+                # color
+
+            # starting point (vertical lines)
+                # self.row * cellsize, (self.col + i) * cellsize
+                # i = 0,1
+                # color
+            # ending point
+                # (self.row + 1) * cellsize, (self.col + i) * cellsize
+                # i = 0,1
+                # color
+
+            # Graphic (if self.value != 0)
+                # x coordinate
+                    # self.col * cellsize + cellsize // 2
+                # y coordinate
+                    # self.row * cellsize + cellsize // 2
